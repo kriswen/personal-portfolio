c3961abb4fb393dc7d98a931862ec9f8abcfe083
kriswen <wen.shaoju@gmail.com>
2024-03-07 19:23:37 -0800

    add back GA tags and added Router basename (for fixing Github page deployment)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       {/*ensure nav routing behavior*/}
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Navbar />
           <Routes>
